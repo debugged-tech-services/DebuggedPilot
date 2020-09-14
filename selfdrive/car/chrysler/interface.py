158796e44e5d8ba34a09ea091074136d61d9bc4d
Andrew Christiansen <andrrocity@gmail.com>
2020-09-14 03:33:25 -0500

    Updating

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -35,8 +35,8 @@ class CarInterface(CarInterfaceBase):
     # ret.steerRateCost = 0.02
     # ret.steerLimitTimer = 0.8
     ret.lateralTuning.init('indi')
-    ret.lateralTuning.indi.innerLoopGain = 0.0092
-    ret.lateralTuning.indi.outerLoopGain = 0.0038
+    ret.lateralTuning.indi.innerLoopGain = 0.092
+    ret.lateralTuning.indi.outerLoopGain = 0.038
     ret.lateralTuning.indi.timeConstant = 1.0;
     ret.lateralTuning.indi.actuatorEffectiveness = 100.0;
 
