e6d8df47ec5a4a4c093d725585720084a4ac7a3b
debugged-llc <mbuboltz@debugged.me>
2020-09-14 14:12:53 -0500

    try .10

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -27,7 +27,7 @@ class CarInterface(CarInterfaceBase):
     ret.wheelbase = 3.089  # in meters for Pacifica Hybrid 2017
     ret.steerRatio = 16.2  # Pacifica Hybrid 2017
     ret.mass = 1964. + STD_CARGO_KG  # kg curb weight Pacifica Hybrid 2017
-    pidscale = 0.098
+    pidscale = 0.10
     ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kfBP = [[9. * pidscale, 20. * pidscale], [9. * pidscale, 20. * pidscale], [0.]]
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kfV = [[0.15 * pidscale,0.30 * pidscale], [0.03 * pidscale,0.05 * pidscale], [0.00006 * pidscale]] # full torque for 10 deg at 80mph means 0.00007818594
     ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.]]
