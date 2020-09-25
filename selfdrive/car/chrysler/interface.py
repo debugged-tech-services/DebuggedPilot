0710acb3af9191f2a9e47e08219e1bb2e8747976
Andrew Christiansen <andrrocity@gmail.com>
2020-09-25 17:16:54 -0500

    Updating INDI

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -47,13 +47,13 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1828.0 + STD_CARGO_KG # 2013 V-6 RWD
       # ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
       ret.steerActuatorDelay =  0.01
-      ret.steerRateCost = 0.02
+      ret.steerRateCost = 0.1
       ret.steerLimitTimer = 0.8
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 2.52
-      ret.lateralTuning.indi.outerLoopGain = 1.28
+      ret.lateralTuning.indi.innerLoopGain = 1.82
+      ret.lateralTuning.indi.outerLoopGain = 0.92
       ret.lateralTuning.indi.timeConstant = 1000.0
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.2971
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.4971
 
     ret.minSteerSpeed = 3.8  # m/s
     if candidate in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
