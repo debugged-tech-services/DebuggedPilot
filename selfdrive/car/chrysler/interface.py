69770130affb969e5152f92dac87426441fa236a
Comma Device <device@comma.ai>
2020-10-09 10:55:23 +0000

    updaingn 300 indi

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -52,15 +52,15 @@ class CarInterface(CarInterfaceBase):
       ret.steerRateCost = 0.002
       ret.steerLimitTimer = 0.8
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 1.348
-      ret.lateralTuning.indi.outerLoopGain = 0.8989
+      ret.lateralTuning.indi.innerLoopGain = 1.148
+      ret.lateralTuning.indi.outerLoopGain = 0.989
       ret.lateralTuning.indi.timeConstant = 10.0
-      ret.lateralTuning.indi.actuatorEffectiveness = 2.10
+      ret.lateralTuning.indi.actuatorEffectiveness = 1.832
       # ret.lateralTuning.indi.innerLoopGain = 3.0
       # ret.lateralTuning.indi.outerLoopGain = 2.0
       # ret.lateralTuning.indi.timeConstant = 1.0
       # ret.lateralTuning.indi.actuatorEffectiveness = 1.5
-      tire_stiffness_factor = 0.444
+      tire_stiffness_factor = 0.65
 
     ret.minSteerSpeed = 3.8  # m/s
     if candidate in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
