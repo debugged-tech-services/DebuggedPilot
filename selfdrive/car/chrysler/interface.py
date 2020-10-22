7462a3a89dccc09cf5200d3855175a7588fed326
debugged-llc <mbuboltz@debugged.me>
2020-10-22 01:57:19 -0500

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -32,14 +32,13 @@ class CarInterface(CarInterfaceBase):
     #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kfV = [[0.15 * pidscale,0.30 * pidscale], [0.03 * pidscale,0.05 * pidscale], [0.00006 * pidscale]] # full torque for 10 deg at 80mph means 0.00007818594
     #ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.1]]
     
-    ret.steerActuatorDelay = 0.02 #steer packet is sent every 20 ms
+    ret.steerActuatorDelay =  0.02 #steer packet is sent every 20 ms
     ret.steerRateCost = 0.002
     ret.steerLimitTimer = 0.8
-
     ret.lateralTuning.init('indi')
     ret.lateralTuning.indi.innerLoopGain = 1.0
-    ret.lateralTuning.indi.outerLoopGainBP = [0, 5, 10] #BP is the vehicle speed in m/sec
-    ret.lateralTuning.indi.outerLoopGainV = [0.5, 1.5, 2.0] #V is the gain value to use when at that speed
+    ret.lateralTuning.indi.outerLoopGainV = [1.0] #V is the gain value to use when at that speed
+    ret.lateralTuning.indi.outerLoopGainBP = [0] #BP is the vehicle speed in m/sec
     ret.lateralTuning.indi.timeConstant = 1.0
     ret.lateralTuning.indi.actuatorEffectiveness = 1.0
 
