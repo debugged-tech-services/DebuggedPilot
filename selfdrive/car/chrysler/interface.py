20d9c96be8e7900a3a7564dd377df87f5f7054c6
debugged-llc <mbuboltz@debugged.me>
2020-10-22 01:09:18 -0500

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -32,7 +32,7 @@ class CarInterface(CarInterfaceBase):
     #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kfV = [[0.15 * pidscale,0.30 * pidscale], [0.03 * pidscale,0.05 * pidscale], [0.00006 * pidscale]] # full torque for 10 deg at 80mph means 0.00007818594
     #ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0.], [0.1]]
     
-    ret.steerActuatorDelay = 50.0
+    ret.steerActuatorDelay = 0.02
     #ret.steerRateCost = 1.0
     ret.steerLimitTimer = 0.8
 
