5b9558eac96c9252d1d9d532dd9cd86c87fea16b
debugged-llc <mbuboltz@debugged.me>
2020-10-22 13:03:15 -0500

    35 -> 33

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -40,7 +40,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.indi.outerLoopGainV = [3.5] #V is the gain value to use when at that speed
     ret.lateralTuning.indi.outerLoopGainBP = [0] #BP is the vehicle speed in m/sec
     ret.lateralTuning.indi.timeConstant = 1.0
-    ret.lateralTuning.indi.actuatorEffectiveness = 35.0
+    ret.lateralTuning.indi.actuatorEffectiveness = 33.0
 
     if candidate in (CAR.JEEP_CHEROKEE_2017, CAR.JEEP_CHEROKEE_2018, CAR.JEEP_CHEROKEE_2019):
       ret.wheelbase = 2.91  # in meters
