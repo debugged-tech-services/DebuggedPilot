a6fe3025efda1679a38bbec99eff69703fbd4114
xps-genesis <aruncool17@gmail.com>
2021-12-20 16:37:50 -0500

    missed ;

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -174,7 +174,7 @@ void OnroadHud::updateState(const UIState &s) {
   const auto cs = sm["controlsState"].getControlsState();
 
   float maxspeed = cs.getVCruise();
-  bool brake_active = sm["carState"].getCarState().getBrakeLights()
+  bool brake_active = sm["carState"].getCarState().getBrakeLights();
   bool cruise_set = maxspeed > 0 && (int)maxspeed != SET_SPEED_NA;
   if (cruise_set && !s.scene.is_metric) {
     maxspeed *= KM_TO_MILE;
