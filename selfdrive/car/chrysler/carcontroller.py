1e665dfa40aadd096a7625bd14e61fe448f40621
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-12-20 19:23:49 -0500

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -315,5 +315,5 @@ class CarController():
 
     new_actuators = actuators.copy()
     new_actuators.steer = apply_steer / CarControllerParams.STEER_MAX
-    new_actuators.accel = self.accel
+    # new_actuators.accel = self.accel
     return new_actuators, can_sends
