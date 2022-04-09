7b898efe5cf11251c4585266adf4af7d3fe76cc9
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2022-04-08 21:10:29 -0400

    Update radar_interface.py

--- selfdrive/car/chrysler/radar_interface.py
+++ selfdrive/car/chrysler/radar_interface.py
@@ -81,7 +81,7 @@ class RadarInterface(RadarInterfaceBase):
         #self.pts[trackId].yRel = math.tan(azimuth) * cpt['LONG_DIST']
       else:  # d_* message
         self.pts[trackId].vRel = cpt['REL_SPEED']
-        self.pts[trackId].measured = cpt['MEASURED']
+        self.pts[trackId].measured = bool(cpt['MEASURED'])
 
     # We want a list, not a dictionary. Filter out LONG_DIST==0 because that means it's not valid.
     ret.points = [x for x in self.pts.values() if x.measured and (255 > x.dRel > 0)]
