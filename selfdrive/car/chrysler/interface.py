3e03fdee2c856cd4f06dd52769957b67a1e2b1ef
debugged-tech <mbuboltz@debuggedtech.com>
2022-12-31 19:22:42 -0500

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -34,7 +34,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kfV = [0.00005]   # full torque for 10 deg at 80mph means 0.00007818594
     else:
       ret.lateralTuning.pid.kpBP = [0., 10., 35.]
-      ret.lateralTuning.pid.kpV = [0.012, 0.015, 0.02] # stock values [0.05, 0.07, 0.075]
+      ret.lateralTuning.pid.kpV = [0.013, 0.016, 0.022] # stock values [0.05, 0.07, 0.075]
 
       ret.lateralTuning.pid.kiBP = [0., 15., 30.]
       ret.lateralTuning.pid.kiV = [0.0025, 0.0028, 0.003] # [0.002, 0.003, 0.0035]
