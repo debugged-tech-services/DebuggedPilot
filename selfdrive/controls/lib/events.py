23bc59d1c68d94de87847ff286c42b01fae3ef6a
debugged-tech <mbuboltz@debuggedtech.com>
2023-05-02 23:54:18 -0500

    try this to stay enabled

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -163,8 +163,8 @@ class UserSoftDisableAlert(SoftDisableAlert):
 class ImmediateDisableAlert(Alert):
   def __init__(self, alert_text_2: str):
     super().__init__("TAKE CONTROL IMMEDIATELY", alert_text_2,
-                     AlertStatus.critical, AlertSize.full,
-                     Priority.HIGHEST, VisualAlert.steerRequired,
+                     AlertStatus.userPrompt, AlertSize.mid,
+                     Priority.LOW, VisualAlert.none,
                      AudibleAlert.refuse, 4.),
 
 
